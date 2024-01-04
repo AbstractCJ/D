308952540fb29c05726a910843ae98bf52009fd1
User <User@DESKTOP-6JPNDEN.lan>
2024-01-04 11:02:13 -0500

    Learning

---
+++ src/D/D.java
@@ -0,0 +1,12 @@
+package D;
+
+public class D {
+	
+public static void main(String[] args) {
+		
+		System.out.println("Let the fourth be with you.");
+		System.out.println("D");
+		System.out.println("");
+	}
+
+}
